9dee0569638bc3d227fe9b79bf2dfb5a25138fc0
ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFCpkaQgLdANfmSqQ65NlnJ1Q592NjLqG5iHhyENjG9HTPNUSdX8y+sjvKnb7h1B1P2s9jPnlVG3aXb22rK30MG0Y4dCLMwL20FMGc7+4jYH70S8OxetWUgxY7y9/5xdmAJ3AWvuuMNf9s0KMKIfYJl85V8kTAub6JsH/pSlCtT1U56Kt1ao0ipV/o6evBsDV/R1X9gfOspgx53DKuwl//goanRAtqXoEURFW/nt4L8DPfBodtMS+OBtIqR3SobFSd0bChP2PxUD5g08QfYyonX1MZHYWJqEq/T8N/K0N9nfuDjEBm62ELvrK4MWosJuwFLOx+UyzyjIRdsx6NSIf+KIOtWLtKx4ldUWzDhrJyN6m3jNT9JRArh1yO61nZfojLgZ6HKoUYSR85MeuZ4ms+sH7WZL7e7Ht5u/gQi5hBT0cvA4Opky8l4uo7tdi9RUMXZKYHUGIZRzUiQAFRjSMdJ8cUTZmhv6OtLDe/NKEa+m5rBrrdyrVJVPP/C8d/Tlk= wdbailey87@gmail.com <wdbailey87@gmail.com>
2021-11-06 17:25:41 -0700

    add books/:id

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,5 @@
 // create an express router
+const { application } = require('express')
 const express = require('express')
 const router = express.Router()
 
@@ -14,8 +15,18 @@ router.get('/', (req, res) => {
 // POST /books -- CREATE one book
 router.post('/', (req, res) => {
   // create a new book with the request body
-  console.log(req.body)
-  res.json({ msg: 'CREATE one book' })
+  const book = req.body.book
+  book._id = books.length
+  books.push(book)
+  // res.json({ book })
+  res.redirect(`/books/${book._id}`) // yes, i created it. you can request it here
+})
+
+// GET /books/:id -- READ one book at :id
+router.get('/:id', (req, res) => {
+  const bookId = req.params.id
+  const selectedBook = books[bookId]
+  res.json({ book: selectedBook })
 })
 
 // export the router
